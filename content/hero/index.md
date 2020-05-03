6a98668fc6528dc6d52c23c6d194971d8f2d06cc
Shannon Ng <shannon95ng@gmail.com>
2020-05-02 22:33:42 -0400

    Changed social links

--- content/hero/index.md
+++ content/hero/index.md
@@ -1,6 +1,6 @@
 ---
 title: 'Hello world!'
-name: 'I’m Shannon Ng'
+name: 'I’m Shannon'
 subtitle: 'Designer based in New York City.'
 buttonText: 'Get In Touch'
 ---
--- src/config.js
+++ src/config.js
@@ -1,38 +1,30 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
-  siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+  siteTitle: 'Shannon Ng | Designer',
+  siteDescription: 'Shannon is a designer based in New York City.',
+  // TODO: Write a description later
   siteKeywords:
     'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
+  // TODO: Write keywords later
   siteUrl: 'https://brittanychiang.com',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Shannon Ng',
+  location: 'New York, NY',
+  email: 'shannon95ng@gmail.com',
+  twitterHandle: '@shannon_95',
   socialMedia: [
     {
-      name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://www.linkedin.com/in/shannon95ng',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/shannon_187',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/shannon_187',
     },
   ],
 
